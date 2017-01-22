4b1db72d67e8df1570cad9210a67117bfb6311f6
isji <sj.balatan@gmail.com>
2017-01-22 22:34:25 +0800

    test commit. added index file

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+        <h1>
+                "Len, I will love you as long as this website is running"
+        </h1>
+</body>
+<footer>
+        <p>** Note: Maintenance downtime scheduled every week.<p>
+</footer>
+</html>
